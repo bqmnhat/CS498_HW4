c4be6bc8e6d23e4f2479c91fb183b74e55a42447
bqmnhat <oliver.nhat.bui@gmail.com>
2026-04-18 18:49:11 -0500

    Update port to run app.py to 8000

--- app.py
+++ app.py
@@ -130,4 +130,4 @@ def company_compare():
     return jsonify({"comparison": [dict(r) for r in results]})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
